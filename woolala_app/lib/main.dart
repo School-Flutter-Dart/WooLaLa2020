474b49e0096bb960cd086d2976c138b818f77650
Hayden Roper <haydenroper@tamu.edu>
2020-10-07 20:59:02 -0500

    file structure updated

--- woolala_app/lib/main.dart
+++ woolala_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:woolala_app/screens/login_screen.dart';
 
 void main() {
   runApp(WooLaLa());
@@ -9,32 +10,13 @@ class WooLaLa extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WooLaLa',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         // This makes the visual density adapt to the platform that you run the app on. For desktop platforms, the controls will be smaller and closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              colors: [
-                Colors.purple[900],
-                Colors.purple[800],
-                Colors.purple[600]
-              ]
-          ),
-        ),
-      ),
+      home: LoginScreen(),
     );
   }
 }
